b83d8d6bb428c34378997a3ed71ababe5b512764
kang JuHye <0julie@naver.com>
2019-03-18 12:21:30 +0900

    Init

---
+++ Project5/c 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void outputPyramid(int n, int k);
+
+int main(void) {
+	int t;
+	int n, k;
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> n >> k;
+		outputPyramid(n, k);
+	}
+	return 0;
+}
+
+void outputPyramid(int n, int k) {
+	for (i=0;)
+
+
+
+}
